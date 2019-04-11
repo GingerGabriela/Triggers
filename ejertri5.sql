8b1e6d78df4f51126f4e00e28ff5316d8ce75ad4
Ginger <gnortiz25@gmail.com>
2019-04-11 12:55:15 +0200

    ejercicios 11-04-2019 /triggers

--- ejertri5.sql
+++ ejertri5.sql
@@ -2,23 +2,19 @@ create or replace trigger insercion after insert on tcliente  for each row
 declare
 	filaven tvendedor%rowtype;
 	pro	 tprovincia%rowtype;
-	--error exception;
-begin
-	begin
+BEGIN
+	BEGIN
 		select * into filaven from tvendedor where cod_ven=:new.vendedor;
 	Exception 
 		when no_data_found then
 			dbms_output.put_line('No existe el vendedor');
-	end;
-	begin
+	END;
+	BEGIN
 		select * into pro from tprovincia where codigo=:new.provincia;	
 	Exception 
 		when no_data_found then
 			dbms_output.put_line('No existe la provincia');
-	end;
---Exception 
-	--when error then
-		--dbms_output.put_line('No existe');
-end;	
+	END;
+END;	
 /
 		
